51bd2252d60f6e3a355aeba701a36344ca0fccef
Ben Mariem Mohamed Ali <dalibm26@gmail.com>
2024-03-17 23:53:58 +0100

    ajouter user

---
+++ src/app/pages/creeuser/creeuser.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-creeuser',
+  templateUrl: './creeuser.component.html',
+  styleUrl: './creeuser.component.scss'
+})
+export class CreeuserComponent {
+
+}
